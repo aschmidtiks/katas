c8f3561f3ebc4a090ebf8158d1fcf536ec6494de
aschmidtiks <main.schmidt.alexander@gmail.com>
2019-07-01 11:32:00 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String input1;
+        Scanner scanner = new Scanner(System.in);
+
+        input1 = scanner.nextLine();
+
+        System.out.println("TEST");
+    }
+}
